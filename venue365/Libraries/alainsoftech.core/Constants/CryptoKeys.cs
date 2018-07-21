8864593b7dae134849b9017ebe55ec7fb01a7d87
Al-ain Softech <abbas.developers@gmail.com>
2018-07-22 04:57:12 +0500

    Update CryptoKeys.cs

--- venue365/Libraries/alainsoftech.core/Constants/CryptoKeys.cs
+++ venue365/Libraries/alainsoftech.core/Constants/CryptoKeys.cs
@@ -8,6 +8,6 @@ namespace alainsoftech.core.Constants
 {
     class CryptoKeys
     {
-        public const string EncryptionKey = "BCF4F476E52DE367B88AF1E49C759BC645C1900677A8D2AEEA42B8F6ED22EE98DDD8BED4B3AC3DAFC615A7B5F51148B0593F0A665F760FBB902B91A09292F336";
+        public const string EncryptionKey = "";
     }
 }
