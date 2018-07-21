b4eb9992d69f1051ed893e482984861e03df9086
Abbas <abbas.developers@outlook.com>
2018-07-22 04:59:35 +0500

    Revert Crypto Keys

---
+++ venue365/Libraries/venue365.securityprovider/Constants/Keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace venue365.securityprovider.Constants
+{
+    public class Keys
+    {
+        public const string EncryptionKey = "";
+    }
+}
