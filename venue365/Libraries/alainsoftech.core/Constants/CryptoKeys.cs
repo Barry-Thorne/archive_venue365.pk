56e917dd773c871634b4c2b5eba831ba8de38cb4
Abbas <abbas.developers@outlook.com>
2018-07-22 04:55:06 +0500

    Keys Reverted

---
+++ venue365/Libraries/alainsoftech.core/Constants/CryptoKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alainsoftech.core.Constants
+{
+    class CryptoKeys
+    {
+        public const string EncryptionKey = "BCF4F476E52DE367B88AF1E49C759BC645C1900677A8D2AEEA42B8F6ED22EE98DDD8BED4B3AC3DAFC615A7B5F51148B0593F0A665F760FBB902B91A09292F336";
+    }
+}
--- venue365/Libraries/venue365.securityprovider/Utilities/Crypto.cs
+++ venue365/Libraries/venue365.securityprovider/Utilities/Crypto.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace venue365.securityprovider.Utilities
 {
     public class Crypto
